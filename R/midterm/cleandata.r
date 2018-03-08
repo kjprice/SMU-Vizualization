66af82be6526fbd3e63814a01bb85cd1377a31e1
KJ <kjprice12@gmail.com>
2018-03-07 21:24:16 -0600

    Midterm - clean data

---
+++ R/midterm/cleandata.r
@@ -0,0 +1,12 @@
+gun.violence = read.csv('data/GunViolence.csv')
+
+# Turn "12,000" into a true number 12000
+gun.violence$GDP = as.integer(gsub(",", "", gun.violence$GDP))
+
+# Turn "1.03 (2016)" into a true number 1.03
+gun.violence$Homicides = as.numeric(gsub("\\s\\([0-9]{4}\\)", "", gun.violence$Homicides))
+gun.violence$Suicides = as.numeric(gsub("\\s\\([0-9]{4}\\)", "", gun.violence$Suicides))
+
+gun.violence$Cultural.Diversity.Index
+
+write.csv(gun.violence, 'data/GunViolenceClean.csv')

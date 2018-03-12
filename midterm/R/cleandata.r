61188a36de6e0d7fb99570cb369e95cc0d901ad8
KJ <kjprice12@gmail.com>
2018-03-11 21:20:05 -0500

    add column "developed_nation"

--- midterm/R/cleandata.r
+++ midterm/R/cleandata.r
@@ -7,8 +7,13 @@ gun.violence$GDP = as.integer(gsub(",", "", gun.violence$GDP))
 gun.violence$Homicides = as.numeric(gsub("\\s\\([0-9]{4}\\)", "", gun.violence$Homicides))
 gun.violence$Suicides = as.numeric(gsub("\\s\\([0-9]{4}\\)", "", gun.violence$Suicides))
 
+
 # Turn into a true number
 gun.violence$Cultural.Diversity.Index = as.numeric(gsub("unavailable", "", gun.violence$Cultural.Diversity.Index))
 gun.violence$Guns.Per.Capita = as.numeric(gsub("unavailable", "", gun.violence$Guns.Per.Capita))
+
+# Create "developed_nation" column
+gun.violence$developed_nation = gun.violence$GDP >= 20000
+
 
 write.csv(gun.violence, '../data/GunViolenceClean.csv', row.names = FALSE)

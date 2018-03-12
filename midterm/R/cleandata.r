ec23574259435bec32de070eacf250d605be26a5
KJ <kjprice12@gmail.com>
2018-03-11 21:54:43 -0500

    fill na values

--- midterm/R/cleandata.r
+++ midterm/R/cleandata.r
@@ -15,4 +15,12 @@ gun.violence$Guns_Per_Capita = as.numeric(gsub("unavailable", "", gun.violence$G
 # Create "developed_nation" column
 gun.violence$developed_nation = gun.violence$GDP >= 20000
 
+# fill na values
+gun.violence$Guns_Per_Capita2[is.na(gun.violence$Guns_Per_Capita)] = median(gun.violence$Guns_Per_Capita[!is.na(gun.violence$Guns_Per_Capita)])
+gun.violence$Homicides2[is.na(gun.violence$Homicides)] = median(gun.violence$Homicides[!is.na(gun.violence$Homicides)])
+gun.violence$Suicides2[is.na(gun.violence$Suicides)] = median(gun.violence$Suicides[!is.na(gun.violence$Suicides)])
+gun.violence$Cultural_Diversity_Index2[is.na(gun.violence$Cultural_Diversity_Index)] = median(gun.violence$Cultural_Diversity_Index[!is.na(gun.violence$Cultural_Diversity_Index)])
+#gun.violence$GDP2[is.na(gun.violence$GDP)] = mean(gun.violence$GDP[!is.na(gun.violence$GDP)])
+
+
 write.csv(gun.violence, '../data/GunViolenceClean.csv', row.names = FALSE)
